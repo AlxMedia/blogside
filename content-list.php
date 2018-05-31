9bd3506a1eb70bde9569c7cb4961dac05fb27a03
Alexander Agnarson <alexander.agnarson@gmail.com>
2018-05-31 18:21:07 +0200

    the_title() changed to the_title_attribute() in html attributes

--- content-list.php
+++ content-list.php
@@ -6,7 +6,7 @@
 				<?php if ( has_post_thumbnail() ): ?>
 					<?php the_post_thumbnail('blogline-list'); ?>
 				<?php elseif ( get_theme_mod('placeholder') != 'off' ): ?>
-					<img src="<?php echo get_template_directory_uri(); ?>/img/thumb-list.png" alt="<?php the_title(); ?>" />
+					<img src="<?php echo get_template_directory_uri(); ?>/img/thumb-list.png" alt="<?php the_title_attribute(); ?>" />
 				<?php endif; ?>
 				<?php if ( has_post_format('video') && !is_sticky() ) echo'<span class="thumb-icon"><i class="fa fa-play"></i></span>'; ?>
 				<?php if ( has_post_format('audio') && !is_sticky() ) echo'<span class="thumb-icon"><i class="fa fa-volume-up"></i></span>'; ?>
