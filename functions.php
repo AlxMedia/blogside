1355058adaf4c8ee8c67cedbf226f15ef934bbc3
Alexander Agnarson <alexander.agnarson@gmail.com>
2021-05-25 13:07:29 +0200

    1.2.2

--- functions.php
+++ functions.php
@@ -90,8 +90,10 @@ add_action( 'after_setup_theme', 'blogside_setup' );
 
 /*  Custom navigation
 /* ------------------------------------ */
-add_action( 'wp', function() {
+if ( ! class_exists( '\Blogside\Nav' ) ) {
 	require_once 'functions/nav.php';
+}
+add_action( 'wp', function() {
 	$nav = new \Blogside\Nav();
 	$nav->enqueue(
 		[
